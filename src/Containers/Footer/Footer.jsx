149e36d5d0cec8a6b202a35b85296f3d2653df5f
Shivam Gupta <100027945+ShivamGupta2007@users.noreply.github.com>
2022-11-20 18:17:55 +0530

    my-app

---
+++ src/Containers/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="tm-footer row tm-mt-small">
+        <div className="col-12 font-weight-light">
+          <p className="text-center text-white mb-0 px-6 large">
+            Copyright © <b>2021</b> All rights reserved. Design:{" "}
+            <a
+              rel="nofollow noopener"
+              href="https://templatemo.com"
+              className="tm-footer-link"
+            >
+              Template Mo
+            </a>
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
